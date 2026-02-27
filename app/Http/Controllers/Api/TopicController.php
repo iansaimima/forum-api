8a846005822eac5383a22628f414113ab63bd316
Ian Ibnu Wahab <iandroid.saimima@gmail.com>
2026-02-27 16:37:20 +0800

    update

--- app/Http/Controllers/Api/TopicController.php
+++ app/Http/Controllers/Api/TopicController.php
@@ -15,7 +15,7 @@ class TopicController extends Controller
     public function index()
     {
         // Get IDs of users that the authenticated user is following
-        $followingIds = auth()->user()->following()->pluck('id');
+        $followingIds = auth()->user()->following()->pluck('users.id');
 
         $topics = Topic::with(['user', 'category:id,name', 'comments', 'likes'])
             ->withCount(['comments', 'likes'])
